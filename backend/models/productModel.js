4ce8c097a6ab0d1256b6845d156a457934029ec3
EmanElBoghdady <emaelboghdady807060504030@gmail.com>
2025-05-06 17:17:54 +0300

    ..

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,9 +1,9 @@
 const mongoose= require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String },
   image: { type: String }, 
-  price: { type: Number, required: true },
+  price: { type: Number},
   description: { type: String },
   category: { type: String },
   stock: { type: Number, default: 0 },
